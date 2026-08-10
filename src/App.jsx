87661f289b08c52757ad55b4a7f2f953f59355db
arshiayr <ramtinyt55@gmail.com>
2026-08-10 14:32:00 +0330

    Set up Vite + React project with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+﻿function App() {
+  return (
+    <div className="bg-black h-screen flex flex-col overflow-hidden">
+      <h1 className="text-white text-3xl font-bold p-6">Spotify</h1>
+    </div>
+  );
+}
+
+export default App;
