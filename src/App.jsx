5b8d7da5dd29250e2d39c05ef9b14fbace01b085
arshiayr <ramtinyt55@gmail.com>
2026-08-22 15:26:00 +0330

    Set up client-side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
+﻿import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Home from "./components/Home/Home";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="bg-black h-screen flex flex-col overflow-hidden">
+    <div className="h-screen w-screen bg-black flex flex-col overflow-hidden">
 
       <Header />
 
-      <div className="flex flex-1 gap-2 p-2 min-h-0">
 
-        <Sidebar />
+      <div className="flex-1 min-h-0 flex gap-2 px-2 pb-2">
 
-        <main className="flex-1 min-w-0 min-h-0 overflow-y-auto bg-[#121212] rounded-lg">
-          <Home />
+        <aside className="w-[420px] shrink-0 h-full">
+          <Sidebar />
+        </aside>
+
+
+        <main
+          className="
+            flex-1
+            min-w-0
+            min-h-0
+            h-full
+            bg-[#121212]
+            rounded-lg
+            overflow-y-auto
+          "
+        >
+          <Routes>
+
+            <Route path="/" element={<Home />} />
+          </Routes>
         </main>
-
       </div>
-
     </div>
   );
 }
